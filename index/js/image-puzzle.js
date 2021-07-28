17bde0396bcb504bd48e39347cd6d2576ef47397
dmsridho <dimasridho69@gmail.com>
2021-07-28 12:16:05 +0800

    initial commit

---
+++ index/js/image-puzzle.js
@@ -0,0 +1,115 @@
+﻿
+var timerFunction;
+
+var imagePuzzle = {
+    stepCount: 0,
+    startTime: new Date().getTime(),
+    startGame: function (images, gridSize) {
+        this.setImage(images, gridSize);
+        $('#playPanel').show();
+        $('#sortable').randomize();
+        this.enableSwapping('#sortable li');
+        this.stepCount = 0;
+        this.startTime = new Date().getTime();
+        this.tick();
+    },
+
+
+
+    // ======== BAGIAN FUNGSI  TIMER ======== //
+    tick: function () {
+        var now = new Date().getTime();
+        var elapsedTime = parseInt((now - imagePuzzle.startTime) / 1000, 10);
+        $('#timerPanel').text(elapsedTime);
+        timerFunction = setTimeout(imagePuzzle.tick, 1000);
+    },
+
+
+
+
+
+    // ======== BAGIAN FUNGSI DRAG & DROP FUNGSI ======== //
+    enableSwapping: function (elem) {
+        $(elem).draggable({
+            snap: '#droppable',
+            snapMode: 'outer',
+            revert: "invalid",
+            helper: "clone"
+        });
+        $(elem).droppable({
+            drop: function (event, ui) {
+                var $dragElem = $(ui.draggable).clone().replaceAll(this);
+                $(this).replaceAll(ui.draggable);
+
+                // ------ GAME OVER ------//
+                currentList = $('#sortable > li').map(function (i, el) { return $(el).attr('data-value'); });
+                if (isSorted(currentList)){
+                    console.log('memunculkan Pop Up, COTTTT !!!!');
+                    $('#modal-kotak , #bg').fadeIn("slow");
+                    $('#actualImageBox').empty().html($('#gameOver').html());
+                } else {
+
+                    // ======== BAGIAN STEP COUNT =========//
+                    var now = new Date().getTime();
+                    imagePuzzle.stepCount++;
+                    $('.stepCount').text(imagePuzzle.stepCount);
+                    $('.timeCount').text(parseInt((now - imagePuzzle.startTime) / 1000, 10));
+                }
+                imagePuzzle.enableSwapping(this);
+                imagePuzzle.enableSwapping($dragElem);
+            }
+        });
+    },
+
+
+
+
+// ======== BAGIAN MULAI FUNGSI UNTUK MENGACAK GAMBAR ====== //
+    setImage: function (images, gridSize) {
+        gridSize = gridSize || 3;
+        var percentage = 100 / (gridSize - 1);
+        var image = images[Math.floor(Math.random() * images.length)];
+        $('#imgTitle').html(image.title);
+        $('#actualImage').attr('src', image.src);
+        $('#actualImagePopUp').attr('src', image.src.substring(0, 7)+'popup'+image.src.substring(7));
+        $('#sortable').empty();
+        for (var i = 0; i < gridSize * gridSize; i++) {
+            var xpos = (percentage * (i % gridSize)) + '%';
+            var ypos = (percentage * Math.floor(i / gridSize)) + '%';
+            var li = $('<li class="item" data-value="' + (i) + '"></li>').css({
+                'background-image': 'url(' + image.src + ')',
+                'background-size': (gridSize * 100) + '%',
+                'background-position': xpos + ' ' + ypos,
+                'width': 400 / gridSize,
+                'height': 400 / gridSize
+            });
+            $('#sortable').append(li);
+        }
+        $('#sortable').randomize();
+    }
+};
+
+// ======== BAGIAN AKHIR FUNGSI UNTUK MENGACAK GAMBAR ======== //
+
+
+function isSorted(arr) {
+    for (var i = 0; i < arr.length - 1; i++) {
+        if (arr[i] != i)
+            return false;
+    }
+    return true;
+}
+
+
+// ======== BAGIAN FUNGSI UNTUK RANDOMIZING ======== //
+$.fn.randomize = function (selector) {
+    var $elems = selector ? $(this).find(selector) : $(this).children(),
+        $parents = $elems.parent();
+
+    $parents.each(function () {
+        $(this).children(selector).sort(function () {
+            return Math.round(Math.random()) - 0.5;
+        }).remove().appendTo(this);
+    });
+    return this;
+};
